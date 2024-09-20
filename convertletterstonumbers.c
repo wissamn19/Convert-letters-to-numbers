67a730b0aff289f0838866de8c7dc0733204fccb
wis <wissamnouiri03@gmail.com>
2024-09-20 22:42:50 +0200

    convert letters to numbers

---
+++ convertletterstonumbers.c
@@ -0,0 +1,329 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{  int chiffres_letters(int n)
+{    switch(n)
+      {
+        case 0:
+         printf("zero");
+          break;
+         case 1:
+          printf("un");
+           break;
+        case 2:
+         printf("deux");
+           break;
+        case 3:
+        printf("trois");
+          break;
+        case 4:
+         printf("quatre");
+          break;
+ case 5:
+         printf("cinq");
+          break;
+        case 6:
+         printf("six");
+          break;
+        case 7:
+         printf("sept");
+          break;
+        case 8:
+         printf("huit");
+          break;
+        case 9:
+         printf("neuf");
+          break;
+          case 10:
+         printf("dix");
+          break;
+          case 11:
+         printf("onze");
+          break;
+          case 12:
+         printf("douze");
+          break;
+          case 13:
+         printf("treize");
+          break;
+          case 14:
+         printf("quatorze");
+          break;
+          case 15:
+         printf("quinze");
+          break;
+          case 16:
+         printf("seize");
+          break;
+          case 17:
+         printf("dix-sept");
+          break;
+          case 18:
+         printf("dix-huit");
+          break;
+          case 19:
+         printf("dix-neuf");
+          break;
+          case 20:
+         printf("vingt");
+          break;
+          case 21:
+         printf("vingt et un");
+          break;
+          case 22:
+         printf("vingt-deux");
+          break;
+          case 23:
+         printf("vingt-trois");
+          break;
+          case 24:
+         printf("vingt-quatre");
+          break;
+          case 25:
+         printf("vingt-cinq");
+          break;
+          case 26:
+         printf("vingt-six");
+          break;
+          case 27:
+         printf("vingt-sept");
+          break;
+          case 28:
+         printf("vingt-huit");
+          break;
+          case 29:
+         printf("vingt-neuf");
+          break;
+          case 30:
+         printf("trente");
+          break;
+ case 31:
+         printf("trente et un");
+          break;
+          case 32:
+         printf("trente-deux");
+          break;
+          case 33:
+         printf("trente-trois");
+          break;
+          case 34:
+         printf("trente-quatre");
+          break;
+          case 35:
+         printf("trente-cinq");
+          break;
+          case 36:
+         printf("trente-six");
+          break;
+          case 37:
+         printf("trente-sept");
+          break;
+          case 38:
+         printf("trente-huit");
+          break;
+          case 39:
+         printf("trente-neuf");
+          break;
+          case 40:
+         printf("quarante");
+          break;
+          case 41:
+         printf("quarante et un");
+          break;
+          case 42:
+         printf("quarante-deux");
+          break;
+          case 43:
+         printf("quarante-trois");
+          break;
+          case 44:
+         printf("quarante-quatre");
+          break;
+          case 45:
+         printf("quarante-cinq");
+          break;
+          case 46:
+         printf("quarante-six");
+          break;
+          case 47:
+         printf("quarante-sept");
+          break;
+          case 48:
+         printf("quarante-huit");
+          break;
+          case 49:
+         printf("quarante-neuf");
+          break;
+          case 50:
+         printf("cinquante");
+          break;
+          case 51:
+         printf("cinquante et un");
+          break;
+          case 52:
+         printf("cinquante-deux");
+          break;
+          case 53:
+         printf("cinquante-trois");
+          break;
+          case 54:
+         printf("cinquante-quatre");
+          break;
+          case 55:
+         printf("cinquante-cinq");
+          break;
+          case 56:
+         printf("cinquante-six");
+          break;
+          case 57:
+         printf("cinquante-sept");
+         break;
+          case 58:
+         printf("cinquante-huit");
+          break;
+          case 59:
+         printf("cinquante-neuf");
+          break;
+          case 60:
+         printf("soixante");
+          break;
+          case 61:
+         printf("soixante et un");
+          break;
+          case 62:
+         printf("soixante-deux");
+          break;
+          case 63:
+         printf("soixante-trois");
+         break;
+          case 64:
+         printf("soixante-quatre");
+          break;
+          case 65:
+         printf("soixante-cinq");
+          break;
+          case 66:
+         printf("soixante-six");
+          break;
+          case 67:
+         printf("soixante-sept");
+          break;
+          case 68:
+         printf("soixante-huit");
+          break;
+          case 69:
+         printf("soixante-neuf");
+          break;
+          case 70:
+         printf("soixante-dix");
+          break;
+          case 71:
+         printf("soixante et onze");
+          break;
+          case 72:
+         printf("soixante-douze");
+          break;
+          case 73:
+         printf("soixante-treize");
+          break;
+          case 74:
+         printf("soixante-quatorze");
+          break;
+          case 75:
+         printf("soixante-quinze");
+          break;
+          case 76:
+         printf("soixante-seize");
+          break;
+          case 77:
+         printf("soixante-dix-sept");
+          break;
+          case 78:
+         printf("soixante-dix-huit");
+          break;
+          case 79:
+         printf("soixante-dix-neuf");
+          break;
+          case 80:
+         printf("quatre-vingts");
+          break;
+          case 81:
+         printf("quatre-vingt-un");
+          break;
+          case 82:
+         printf("quatre-vingt-deux");
+          break;
+          case 83:
+         printf("quatre-vingt-trois");
+          break;
+          case 84:
+         printf("quatre-vingt-quatre");
+          break;
+          case 85:
+         printf("quatre-vingt-cinq");
+          break;
+          case 86:
+         printf("quatre-vingt-six");
+          break;
+          case 87:
+         printf("quatre-vingt-sept");
+          break;
+          case 88:
+         printf("quatre-vingt-huit");
+          break;
+          case 89:
+         printf("quatre-vingt-neuf");
+          break;
+          case 90:
+         printf("quatre-vingt-dix");
+          break;
+          case 91:
+         printf("quatre-vingt-onze");
+          break;
+          case 92:
+         printf("quatre-vingt-douze");
+          break;
+          case 93:
+         printf("quatre-vingt-treize");
+          break;
+          case 94:
+         printf("quatre-vingt-quatorze");
+          break;
+          case 95:
+         printf("quatre-vingt-quinze");
+          break;
+          case 96:
+         printf("quatre-vingt-seize");
+          break;
+          case 97:
+         printf("quatre-vingt-dix-sept");
+          break;
+          case 98:
+         printf("quatre-vingt-dix-huit");
+          break;
+          case 99:
+         printf("quatre-vingt-dix-neuf");
+          break;
+          case 100:
+         printf("cent");
+          break;
+          default:
+           printf("je n'ai aucun reponse pour ton nombre");
+           break;
+
+
+ }
+      return n;
+}
+
+int x ;
+      printf("donne un monbre x:");
+      scanf("%d",&x);
+       chiffres_letters(x);
+
+
+  return x;
+
+
+}
